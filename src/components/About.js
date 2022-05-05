6af1f4ede5e2e682819a062fffca55c57b90d28e
RayyaneElmalhouni <elmalhounirayyane@gmail.com>
2022-05-06 01:08:45 +0200

    Deployed app

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,9 @@ function Lessons() {
       <div className="content">
         <p>
           Nous sommes une équipe qui va vous aidez à apprendre les mathématiques
-          sans aucun problème, offrant des leçons gratuite pour tout public.
+          sans aucun problème, offrant des leçons gratuites pour tout public.
           Nous offrant aussi des séries d'éxercices qui peuvent vous aidez.Vous
-          pouvez accédez à touts ses offres à partir du siteweb{" "}
+          pouvez accédez à toutes ses offres à partir du siteweb{" "}
           <Link to="/">{name}</Link> .
         </p>
       </div>
